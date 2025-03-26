506ef37fefdb41ab02ce9d7fa45fc30155d7cc15
Alex Wangondu <140256995+CycloneAGWangondu@users.noreply.github.com>
2025-03-26 09:55:34 +0300

    Add new components and update Navbar

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
